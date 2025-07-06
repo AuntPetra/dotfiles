91916abeed520ac0769d8f8d8dc1a759295ca219
AuntPetra <pedroprimo2010@hotmail.com>
2025-07-06 12:38:52 -0700

    Added javascript related files to completions

---
+++ config/nvim/lua/plugins/devicons.lua
@@ -0,0 +1,12 @@
+
+return {
+  "nvim-tree/nvim-web-devicons",
+  config = function()
+    require("nvim-web-devicons").setup({
+      -- your devicon config here
+      -- for example, you can set the default icon for unknown files
+      -- default = true,
+    })
+  end,
+}
+
